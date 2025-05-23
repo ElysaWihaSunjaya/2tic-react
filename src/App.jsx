fc10c6208c6a0e5c5317230518280806d2aafb88
ElysaWihaSunjaya <elysa23ti@mahasiswa.pcr.ac.id>
2025-05-23 09:55:26 +0700

    Latihan

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, useState } from 'react'
+import React from 'react';
+import './assets/tailwind.css';
+import Loading from './components/Loading';
+import {Routes, Route} from "react-router-dom"
+// import Dashboard from "./pages/Dashboard";
+// import Orders from './pages/Orders';
+// import Customers from './pages/Customer';
+// import NotFound from './pages/NotFound';
+// import OrderForm from "./form/orderform";
+// import CustomerForm from "./form/customerform";
+// import MainLayout from './layouts/MainLayout';
+// import AuthLayout from './layouts/AuthLayout';
+// import Login from './pages/auth/Login';
+// import Register from './pages/auth/Register';
+// import Forgot from './pages/auth/Forgot';
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+const Orders = React.lazy(() => import("./pages/Orders"))
+const Customers = React.lazy(() => import("./pages/Customer"))
+const User = React.lazy(() => import("./pages/User"))
+const NotFound = React.lazy(() => import("./pages/NotFound"))
+const OrderForm = React.lazy(() => import("./form/orderform"))
+const CustomerForm = React.lazy(() => import("./form/customerform"))
+const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+const GuestLayout = React.lazy(() => import("./layouts/GuestLayout"))
+const Login = React.lazy(() => import("./pages/auth/Login"))
+const Register = React.lazy(() => import("./pages/auth/Register"))
+const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
+const CekAnggota = React.lazy(() => import("./form/cekAnggota"))
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Suspense fallback={<Loading />}>
+    <Routes>
+        <Route element={<MainLayout/>}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/User" element={<User />} />
+          <Route path="/orderform" element={<OrderForm />} />
+          <Route path="/customerform" element={<CustomerForm />} />
+          <Route path="/400" element={<NotFound code="400"/>} />
+          <Route path="/401" element={<NotFound code="401"/>} />
+          <Route path="/403" element={<NotFound code="403"/>} />
+        </Route>
+
+        <Route element={<AuthLayout/>}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/forgot" element={<Forgot/>} />
+        </Route>
+
+<Route path="/guest" element={<GuestLayout />}>
+</Route>
+        <Route path="/cekAnggota" element={<CekAnggota/>}>
+        </Route>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    </Suspense>
   )
 }
 
